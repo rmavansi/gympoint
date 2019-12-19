94abd6b23bca2d5d07d06991b94a10dc3fa89eb2
Renan <rmavansi@gmail.com>
2019-12-19 16:05:24 -0300

    yarn workspace removed

---
+++ mobile/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Routes } from './routes';
+
+// import { Container } from './styles';
+
+export default function App() {
+  return <Text>RTe</Text>;
+}
---
+++ mobile/src/routes.js
@@ -0,0 +1,8 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import SignIn from './pages/SignIn';
+
+export default createAppContainer(
+  createSwitchNavigator({
+    SignIn,
+  })
+);
